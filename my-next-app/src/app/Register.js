dcaa32c7c41db95ca042a1cfa133ac167797fa7a
zahraaAed <zahraaalaaeddine94@gmail.com>
2025-03-13 08:21:57 +0200

    adding register page

---
+++ my-next-app/src/app/Register.js
@@ -0,0 +1,90 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+
+const RegisterPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/users/register`, {
+        name,
+        address,
+        email,
+        password,
+      });
+
+      if (response.status === 201) {
+        router.push('/login');
+      }
+    } catch (err) {
+      setError('Unable to sign up');
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <form onSubmit={handleSubmit} className="bg-white p-6 rounded shadow-md">
+        <h2 className="text-2xl mb-4">Register</h2>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <div className="mb-4">
+          <label className="block mb-1" htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            className="border rounded w-full p-2"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1" htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="border rounded w-full p-2"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1" htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="border rounded w-full p-2"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block mb-1" htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+            className="border rounded w-full p-2"
+          />
+        </div>
+        <button type="submit" className="bg-[#4A8C8C] text-white py-2 px-4 rounded">
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
