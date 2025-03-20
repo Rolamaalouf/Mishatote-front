c6e80c709366a46db9dd6f4bdff6f8a86d1dd863
zahraaAed <zahraaalaaeddine94@gmail.com>
2025-03-20 11:47:42 +0200

    adding order item back

--- my-next-app/src/app/Components/adminSidebar.js
+++ my-next-app/src/app/Components/adminSidebar.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 const AdminSidebar = () => {
   return (
     <aside className="w-64 h-screen bg-[#4A8C8C]  text-white p-5 fixed">
-    <aside className="w-64 h-screen bg-[#4A8C8C]  text-white p-5 fixed">
       <h2 className="text-xl font-bold mb-4">Admin Dashboard</h2>
       <ul className="space-y-4">
         <li>
