64c5f8dbeab78d326f95a975efeeb6cce529179b
Rolamaalouf <roula.maalouf@hotmail.fr>
2025-03-24 10:33:53 +0200

    header adjustment

--- my-next-app/src/app/Components/header.js
+++ my-next-app/src/app/Components/header.js
@@ -4,19 +4,16 @@ import Link from "next/link";
 import ImageComponent from "./imageComponent";
 import CartButton from "../Components/CartButton";
 
-
 const Header = () => {
   const { user, logout } = useAuth(); // Get user and logout function
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  //const [isCartOpen, setIsCartOpen] = useState(false); // State for cart popup
-
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-  //const toggleCart = () => setIsCartOpen(!isCartOpen); // Toggle cart visibility
-
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
   return (
-    <header className="top-0 z-50 w-full flex justify-between items-center px-4 md:px-10 py-4 md:py-6 bg-white shadow-md fixed ">
+    <header className="top-0 z-50 w-full flex justify-between items-center px-4 md:px-10 py-4 md:py-6 bg-white shadow-md fixed">
       {/* Logo */}
       <div className="flex items-center">
         <Link href="/">
@@ -59,7 +56,7 @@ const Header = () => {
       </div>
 
       {/* Icons as Buttons */}
-      <div className="flex space-x-4 items-center">
+      <div className="flex space-x-4 items-center relative">
         {!user ? (
           <Link href="/login">
             <ImageComponent 
@@ -70,22 +67,26 @@ const Header = () => {
             />
           </Link>
         ) : (
-          <button 
-            onClick={logout}
-            className="bg-white-500 text-black px-4 py-2 rounded-md text-sm font-medium hover:bg-red-600 transition duration-300"
-          >
-            Logout
-          </button>
+          <div className="relative flex flex-col items-center">
+            {isDropdownOpen && (
+              <div className="absolute top-[-120%] right-0 w-48 bg-white border rounded shadow-md z-50">
+                <button onClick={logout} className="block w-full text-left px-4 py-2 hover:bg-gray-200">Logout</button>
+                {user.role === "admin" && (
+                  <Link href="/admin" className="block px-4 py-2 hover:bg-gray-200">Admin Panel</Link>
+                )}
+                <Link href="/orderHistory" className="block px-4 py-2 hover:bg-gray-200">Orders History</Link>
+              </div>
+            )}
+            <button onClick={toggleDropdown} className="focus:outline-none">
+              <ImageComponent 
+                src="https://i.ibb.co/z04cdbb/image-13-1.png" 
+                alt="User" 
+                width={25} 
+                height={25} 
+              />
+            </button>
+          </div>
         )}
-
-        <Link href="/wishlist">
-          <ImageComponent 
-            src="https://i.ibb.co/spm9LYcK/Vector.png" 
-            alt="Wishlist" 
-            width={25} 
-            height={25} 
-          />
-        </Link>
 
         <Link href="/cart">
           <ImageComponent 
